27f1f54e13380e40a56d1850a6c9e1eea90b16c8
woodeneels <woodeneels@users.noreply.github.com>
2017-03-10 16:22:21 +0000

    added deckbox buylist parsing script

---
+++ deckboxParseNotOwned.py
@@ -0,0 +1,17 @@
+#! python3
+# deckboxParseNotOwned.py - parses the contents of the clipboard into a usable card list.
+#                           hit "buy not owned" on a deckbox list and copy the list to the clipboard,
+#                           then run this script to print out a list that can be imported into e.g. mcm.
+
+import pyperclip, re
+
+listRegex = re.compile(r'\t(\d{0,}\t.+?(?=\t))')
+
+# find matches
+cards = str(pyperclip.paste())
+mo = listRegex.search(cards)
+matches = ''
+for group in listRegex.findall(cards):
+    matches += group.replace('\t', ' ') + '\n'
+
+print(matches)
